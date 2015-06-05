198785aa139e2af16535d749c52285b5abe0ba7f
Jacob Stanley <jacob.stanley@purenet.co.uk>
2015-06-05 11:44:16 +0100

    Added phpstorm to path

--- install-dev.sh
+++ install-dev.sh
@@ -1,10 +1,13 @@
 # Install PHP and nginx stuff
 
-sudo apt-get install nginx
-sudo apt-get install php5-fpm
+sudo apt-get -y install nginx
+sudo apt-get -y install php5-fpm
 
 # Install PHPSTORM
 wget http://download-cf.jetbrains.com/webide/PhpStorm-8.0.3.tar.gz
 tar xfz PhpStorm-8*.tar.gz
+# should i remove first?
 
-# TODO move to the /opt directory and link to /usr/local/bin/
+# adding phpstorm to bin so executable as "phpstorm"
+sudo mv PhpStorm-*/ /opt/phpstorm
+sudo ln -s /opt/phpstorm/bin/phpstorm.sh /usr/local/bin/phpstorm
