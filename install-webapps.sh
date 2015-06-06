208a2f0296a5a235c5c6d1f64bfd0beacb590eda
Jake Stanley <madstanners@icloud.com>
2015-06-06 14:22:41 +0100

    Added appropriate chmod to install-webapps

--- install-webapps.sh
+++ install-webapps.sh
@@ -1,5 +1,6 @@
 # Copies and installs web apps. Doesn't work at the moment TODO fix
 
+chmod +x ~/conf/apps/*
 sudo cp ~/conf/apps/evernote /usr/local/bin/evernote
 sudo cp ~/conf/apps/todoist /usr/local/bin/todoist
 sudo cp ~/conf/apps/tweetdeck /usr/local/bin/tweetdeck
