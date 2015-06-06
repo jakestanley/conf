86ad5f849096a2e9bded494d32ce0e8a5e4355a3
Jake Stanley <madstanners@icloud.com>
2015-06-06 14:13:00 +0100

    Added IntelliJ and Sublime Text to install-opt.sh

--- install-opt.sh
+++ install-opt.sh
@@ -1,2 +1,17 @@
 # Installs the stuff that can't be installed with apt
-sudo apt-get install spotify-client
+
+## IntelliJ - Keep updated with latest IntelliJ version. Is there a way to automate this?
+cd ~
+wget http://download.jetbrains.com/idea/ideaIC-14.1.3.tar.gz
+tar xvf ideaIC*
+rm idea*tar.gz
+sudo mv idea*/ /opt/intellij
+sudo ln -s /opt/intellij/bin/idea.sh /usr/local/bin/intellij
+
+## Sublime Text
+cd ~
+wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2
+tar xvf Sublime*
+rm Sublime*tar.bz2
+sudo mv Sublime*/ /opt/sublime
+sudo ln -s /opt/sublime/sublime_text /usr/local/bin/sublime_text
