1b9648930ecb5e7e96ede7fd5026459898ef0ac4
Jake Stanley <madstanners@icloud.com>
2015-06-06 15:04:45 +0100

    added magerun, removed phpstorm from install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -1,13 +1,10 @@
 # Install PHP and nginx stuff
-
 sudo apt-get -y install nginx
 sudo apt-get -y install php5-fpm
+sudo apt-get -y install php5-cli
 
-# Install PHPSTORM
-wget http://download-cf.jetbrains.com/webide/PhpStorm-8.0.3.tar.gz
-tar xfz PhpStorm-8*.tar.gz
-# should i remove first?
-
-# adding phpstorm to bin so executable as "phpstorm"
-sudo mv PhpStorm-*/ /opt/phpstorm
-sudo ln -s /opt/phpstorm/bin/phpstorm.sh /usr/local/bin/phpstorm
+# Install n98-magerun.phar
+cd ~
+curl -sS http://files.magerun.net/n98-magerun-latest.phar -o n98-magerun.phar
+chmod +x n98-*
+sudo mv n98-* /usr/local/bin/magerun
