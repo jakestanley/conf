cccfbc444f5e4140ae268b28133235781cb122fd
Jake Stanley <madstanners@icloud.com>
2015-06-06 18:51:05 +0100

    added -y flag to chrome installer

--- install-chrome.sh
+++ install-chrome.sh
@@ -1,6 +1,6 @@
 # Installs Google Chrome
 cd ~
-sudo apt-get install libxss1 libappindicator1 libindicator7
+sudo apt-get install -y libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 rm google-chrome*.deb
