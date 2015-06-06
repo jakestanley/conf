e6d66b6d7c9b18b1d5291e0f7180698871948336
Jake Stanley <madstanners@icloud.com>
2015-06-06 15:27:40 +0100

    Changed order slightly

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -1,9 +1,9 @@
 ## Remove existing i3 configuration (TODO if exists?)
-sudo rm ~/.i3 -r
 sudo mv ~/.bashrc ~/.bashrc.default
 sudo ln -s ~/conf/dotfiles/.bashrc ~/.bashrc
 
 # Symlink to i3 configuration in Dropbox
+sudo rm ~/.i3 -r
 sudo ln -s ~/conf/dotfiles/.i3 ~/.i3
 
 ## need to link dotfiles for .profile and some other crap
