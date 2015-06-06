60a58941fea64222d694b47602dc878765669b28
Jake Stanley <madstanners@icloud.com>
2015-06-06 12:34:49 +0100

    Added sudo to bootstrap.sh where necessary

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Downloads and installs Git on a vanilla Ubuntu installation and checks out the project. Run as superuser
-apt-get update
-apt-get install -y git
+sudo apt-get update
+sudo apt-get install -y git
 cd ~
 git clone https://github.com/madstanners/conf.git
