ec5e6460ce1b2ca96f67c0c0b3603459a1e3eaac
Jake Stanley <madstanners@icloud.com>
2015-06-06 13:32:25 +0100

    Added install-chrome.sh

--- install-chrome.sh
+++ install-chrome.sh
@@ -3,3 +3,4 @@
 sudo apt-get install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
+rm google-chrome*.deb
