19849bc474fed560071686afb3053d9e6d9ca88f
Jake Stanley <madstanners@icloud.com>
2015-06-06 15:56:46 +0100

    erm

--- install-all.sh
+++ install-all.sh
@@ -1,1 +1,39 @@
 # Installs and sets up everything, pretty much
+
+# Make sure we're in the directory
+
+cd ~/conf
+./clean-home.sh
+
+cd ~/conf
+./add-repos
+
+cd ~/conf
+./install-core.sh
+
+cd ~/conf
+./install-chrome.sh
+
+cd ~/conf
+./install-dev.sh
+
+cd ~/conf
+./install-i3.sh
+
+cd ~/conf
+./install-opt.sh
+
+cd ~/conf
+./install-optional.sh
+
+cd ~/conf
+./install-samba.sh
+
+cd ~/conf
+./install-web.sh
+
+cd ~/conf
+./install-webapps.sh
+
+cd ~/conf
+./link-dotfiles.sh
