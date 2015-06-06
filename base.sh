1ef7847258ae66f09fc3f311eb51e8da28e186d6
Jake Stanley <madstanners@icloud.com>
2015-06-06 11:49:04 +0100

    Added base script.
    This will be uploaded to the server

---
+++ base.sh
@@ -0,0 +1,5 @@
+# Downloads and installs Git on a vanilla Ubuntu installation and checks out the project. Run as superuser
+apt-get update
+apt-get install -y git
+cd ~
+git clone https://github.com/madstanners/conf.git
