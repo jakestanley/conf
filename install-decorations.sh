8ff2e25e747aed1238c0427542b396e7f877ccbf
Jake Stanley <madstanners@icloud.com>
2015-06-06 13:40:10 +0100

    renamed install-decorations.sh to install-optional.sh

--- install-decorations.sh
+++
@@ -1,3 +0,0 @@
-# install decorative stuff
-## TODO "mkdir ~/wallpapers" and handle that a decent way with i3 config
-sudo apt-get -y install feh
---
+++ install-optional.sh
@@ -0,0 +1,4 @@
+# install optional stuff
+## TODO "mkdir ~/wallpapers" and handle that a decent way with i3 config
+sudo apt-get -y install feh
+sudo apt-get install spotify-client
