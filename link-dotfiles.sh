1175b81e697793e1ab03f3ab18dc23e1ed5c3796
Jake Stanley <madstanners@icloud.com>
2015-06-06 15:14:07 +0100

    Added .bashrc link

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -1,5 +1,7 @@
 ## Remove existing i3 configuration (TODO if exists?)
 sudo rm ~/.i3 -r
+sudo mv ~/.bashrc ~/.bashrc.default
+sudo ln -s ~/conf/dotfiles/.bashrc ~/.bashrc
 
 # Symlink to i3 configuration in Dropbox
 sudo ln -s ~/conf/dotfiles/.i3 ~/.i3
