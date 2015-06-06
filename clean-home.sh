6f2af985926f9492479adc846b0ef0406bcc8716
Jake Stanley <madstanners@icloud.com>
2015-06-06 14:51:21 +0100

    Populated clean-home

--- clean-home.sh
+++ clean-home.sh
@@ -1,1 +1,11 @@
-clean-home.sh
+# Removes preinstalled bs folders from home directory
+
+rm -r ~/Desktop
+rm -r ~/Documents
+rm -r ~/Downloads
+rm -r ~/Pictures
+rm -r ~/Music
+rm -r ~/Templates
+rm -r ~/Public
+rm -r ~/Videos
+rm examples.desktop
