30cf252f1e62b0783d98a5aa83f8e8b03c74d4c6
Jake Stanley <madstanners@icloud.com>
2015-06-07 20:48:45 +0100

    Added chmod +x to screenfetch script

--- install-screenfetch.sh
+++ install-screenfetch.sh
@@ -2,4 +2,5 @@ sudo apt-get install -y lsb-release scrot
 mkdir ~/screenfetch
 cd ~/screenfetch
 wget -O screenfetch 'https://raw.github.com/KittyKatt/screenFetch/master/screenfetch-dev'
+sudo chmod +x screenfetch
 sudo cp screenfetch /usr/local/bin/screenfetch
