5f2e1a784c716e63bcc3f611ac2fbd917d91eb1b
Jake Stanley <madstanners@icloud.com>
2015-06-07 20:56:59 +0100

    now installing other programs in /usr/local

--- add-repos.sh
+++ add-repos.sh
@@ -4,6 +4,4 @@
 sudo echo "deb http://repository.spotify.com stable non-free" | sudo tee -a /etc/apt/sources.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
 
-
-
 sudo apt-get update
--- install-usr-local.sh
+++ install-usr-local.sh
@@ -5,23 +5,23 @@ cd ~
 wget http://download.jetbrains.com/idea/ideaIC-14.1.3.tar.gz
 tar xvf ideaIC*
 rm idea*tar.gz
-sudo mv idea*/ /opt/intellij
-sudo ln -s /opt/intellij/bin/idea.sh /usr/local/bin/intellij
+sudo mv idea*/ /usr/local/intellij
+sudo ln -s /usr/local/intellij/bin/idea.sh /usr/local/bin/intellij
 
 ## Sublime Text
 cd ~
 wget http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2
 tar xvf Sublime*
 rm Sublime*tar.bz2
-sudo mv Sublime*/ /opt/sublime
-sudo ln -s /opt/sublime/sublime_text /usr/local/bin/sublime_text
+sudo mv Sublime*/ /usr/local/sublime
+sudo ln -s /usr/local/sublime/sublime_text /usr/local/bin/sublime_text
 
 ## PhpStorm
 cd ~
 wget http://download.jetbrains.com/webide/PhpStorm-8.0.3.tar.gz
 tar xvf PhpStorm-*
 rm PhpStorm-8*tar.gz
-sudo mv PhpStorm-*/ /opt/phpstorm/
-sudo ln -s /opt/phpstorm/bin/phpstorm.sh /usr/local/bin/phpstorm
+sudo mv PhpStorm-*/ /usr/local/phpstorm/
+sudo ln -s /usr/local/phpstorm/bin/phpstorm.sh /usr/local/bin/phpstorm
 
 ## TeamSpeak
