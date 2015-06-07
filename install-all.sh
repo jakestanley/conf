7dbc2b49dd211ff4ac656abc1611541ac9a8b611
Jake Stanley <madstanners@icloud.com>
2015-06-07 20:59:00 +0100

    Added rest of scripts to install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -21,7 +21,10 @@ cd ~/conf
 ./install-i3.sh
 
 cd ~/conf
-./install-opt.sh
+./install-misc.sh
+
+cd ~/conf
+./install-usr-local.sh
 
 cd ~/conf
 ./install-optional.sh
@@ -30,10 +33,16 @@ cd ~/conf
 ./install-samba.sh
 
 cd ~/conf
+./install-screenfetch.sh
+
+cd ~/conf
 ./install-web.sh
 
 cd ~/conf
 ./install-webapps.sh
+
+cd ~/conf
+./install-mysql.sh
 
 cd ~/conf
 ./link-dotfiles.sh
