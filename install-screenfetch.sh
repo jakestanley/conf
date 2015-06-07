2108acc480c9539f743c20fc17c248868d701b69
Jake Stanley <madstanners@icloud.com>
2015-06-07 20:39:48 +0100

    Added script to install screenfetch

---
+++ install-screenfetch.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y lsb-release scrot
+mkdir ~/screenfetch
+cd ~/screenfetch
+wget -O screenfetch 'https://raw.github.com/KittyKatt/screenFetch/master/screenfetch-dev'
+sudo cp screenfetch /usr/local/bin/screenfetch
