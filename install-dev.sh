3cbae7edbe04b66b1b97b9b1404d1711486bd698
Jacob Stanley <jacob.stanley@purenet.co.uk>
2015-06-08 10:04:06 +0100

    Added php module dependencies for magento

--- install-dev.sh
+++ install-dev.sh
@@ -3,6 +3,12 @@ sudo apt-get -y install nginx
 sudo apt-get -y install php5-fpm
 sudo apt-get -y install php5-cli
 sudo apt-get -y install php5-mysql
+sudo apt-get -y install php5-gd
+sudo apt-get -y install php5-curl
+sudo apt-get -y install php5-mcrypt
+
+# ln mycrypt
+sudo ln -s /etc/php5/mods-available/mcrypt.ini /etc/php5/cli/conf.d/20-mcrypt.ini
 
 # Install n98-magerun.phar
 cd ~
