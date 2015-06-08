c98d98a2fa931a1cd2972133c9417c54702bfa34
Jacob Stanley <jacob.stanley@purenet.co.uk>
2015-06-08 11:49:42 +0100

    Removed directory on install-screenfetch.sh
    Need to optimise this

--- install-screenfetch.sh
+++ install-screenfetch.sh
@@ -4,3 +4,4 @@ cd ~/screenfetch
 wget -O screenfetch 'https://raw.github.com/KittyKatt/screenFetch/master/screenfetch-dev'
 sudo chmod +x screenfetch
 sudo cp screenfetch /usr/local/bin/screenfetch
+sudo rm -r ~/screenfetch
