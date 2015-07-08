6c2b141441ee47e0a06e6784e07261a2e8ce82fb
jacobstanley <jacob.stanley@purenet.co.uk>
2015-07-08 17:42:52 +0100

    Added kdiff3 to install-misc

--- install-misc.sh
+++ install-misc.sh
@@ -2,4 +2,5 @@ sudo apt-get install -y evince
 sudo apt-get install -y pandoc
 sudo apt-get install -y pidgin
 sudo apt-get install -y gitg
+sudo apt-get install -y kdiff3
 sudo apt-get install -y tree
