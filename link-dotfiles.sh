cd4e11f5620e0659e4aa503632375454ad9bd687
jacobstanley <jacob.stanley@purenet.co.uk>
2015-07-08 17:41:10 +0100

    Added .gitconfig

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -9,3 +9,7 @@ sudo ln -s ~/conf/dotfiles/.i3 ~/.i3
 # Link to .gconf (gnome configurations)
 sudo rm ~/.gconf -r
 sudo ln -s ~/conf/dotfiles/.gconf ~/.gconf
+
+# Link to .gitconfig
+sudo rm ~/.gitconfig
+sudo ln -s ~/conf/dotfiles/.gitconfig ~/.gitconfig
