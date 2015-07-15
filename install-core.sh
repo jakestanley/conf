ea687caddf8c6613a2d22ae9b5864f627b4d8504
jacobstanley <jacob.stanley@purenet.co.uk>
2015-07-15 09:44:42 +0100

    Added Thunderbird

--- install-core.sh
+++ install-core.sh
@@ -3,3 +3,8 @@
 sudo apt-get -y install vim
 sudo apt-get -y install screen
 sudo apt-get -y install default-jdk
+
+# Install email client
+sudo apt-get -y install thunderbird
+
+# Install window manager and dependencies
