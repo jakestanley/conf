31b1f74f57154c1f07cbe6de9235ddc2ed4c7459
jacobstanley <jacob.stanley@purenet.co.uk>
2015-07-18 00:11:23 +0100

    added -f option for more silent linkages

--- link.sh
+++ link.sh
@@ -1,7 +1,7 @@
 # Script for linking together a bunch of files for my standard PC ubuntu environment
 
 # Build an i3 config file for use with a standard keyboard
-rm -r ~/.i3
+rm -rf ~/.i3
 mkdir -p ~/.i3
 
 cd ~/conf/modules
@@ -11,17 +11,17 @@ cat i3config_header i3config_wallpaper i3config_gaps i3config_keys_standard i3co
 cat i3status_header i3status_general_colour i3status_order_standard i3status_components > ~/.i3/.i3status.conf
 
 # Build a bashrc file for pc
-rm ~/.bashrc
+rm ~/.bashrc -rf
 cat bashrc_general bashrc_keys_standard > ~/.bashrc
 
 # Link to .gconf (gnome configurations)
-sudo rm ~/.gconf -r
+sudo rm ~/.gconf -rf
 sudo ln -s ~/conf/dotfiles/.gconf ~/.gconf
 
 # Link to .gitconfig
-sudo rm ~/.gitconfig
+sudo rm ~/.gitconfig -rf
 sudo ln -s ~/conf/dotfiles/.gitconfig ~/.gitconfig
 
 # Link to .shutter
-sudo rm ~/.shutter -r
+sudo rm ~/.shutter -rf
 sudo ln -s ~/conf/dotfiles/.shutter ~/.shutter
