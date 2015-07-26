ad87758a0b3eebb02208d6afaa9b58d58f44df7a
jacobstanley <jacob.stanley@purenet.co.uk>
2015-07-26 17:27:24 +0100

    Added rofi and i3config shortcuts

---
+++ install-rofi.sh
@@ -0,0 +1,13 @@
+# Script for installing [Rofi](https://davedavenport.github.io/rofi/) 
+
+# Install dependencies
+sudo apt-get install libxinerama-dev
+
+# Download and make
+cd ~
+wget https://github.com/DaveDavenport/rofi/releases/download/0.15.7/rofi-0.15.7.tar.gz
+tar -zxvf rofi*
+cd ~/rofi*
+./configure
+sudo make
+sudo make install
