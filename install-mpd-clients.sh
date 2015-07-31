fdec0bd4decb3d756aaae7459159492c5e789824
jacobstanley <jacob.stanley@purenet.co.uk>
2015-07-31 01:02:24 +0100

    added some -y's

--- install-mpd-clients.sh
+++ install-mpd-clients.sh
@@ -11,4 +11,4 @@ sudo cp ~/clerk/clerk /usr/local/bin/
 sudo cp ~/clerk/clerk_helper /usr/local/bin/
 
 # Because there's something I like about ncmpcpp
-sudo apt-get install ncmpcpp
+sudo apt-get install -y ncmpcpp
--- install-mpd.sh
+++ install-mpd.sh
@@ -1,6 +1,6 @@
 # For installing Mopidy.
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
 sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/mopidy.list
-sudo apt-get update
-# sudo apt-get install mopidy
-sudo apt-get install mopidy-spotify
+sudo apt-get update 
+sudo apt-get install -y mopidy
+sudo apt-get install -y mopidy-spotify
