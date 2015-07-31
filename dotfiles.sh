7871543f6de54287cf9db6eb0858f6a21e469130
jacobstanley <jacob.stanley@purenet.co.uk>
2015-08-01 00:47:33 +0100

    removed gconf stuff from dotfiles linker script

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,7 +1,6 @@
 rm ~/.bash_logout
 rm ~/.bashrc
 rm ~/.config
-rm ~/.gconf
 rm ~/.gitconfig
 rm ~/.i3
 rm ~/.inputrc
@@ -15,7 +14,6 @@ rm ~/.Xresources
 ln -s ~/conf/dotfiles/.bash_logout ~/.bash_logout
 ln -s ~/conf/dotfiles/.bashrc ~/.bashrc
 ln -s ~/conf/dotfiles/.config ~/.config
-ln -s ~/conf/dotfiles/.gconf ~/.gconf
 ln -s ~/conf/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/conf/dotfiles/.gtkrc-2.0 ~/.gtkrc-2.0
 ln -s ~/conf/dotfiles/.i3 ~/.i3
