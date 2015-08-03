ef7fab4f8ee520c855b714b5335888dd264cfc66
jacobstanley <jacob.stanley@purenet.co.uk>
2015-08-03 15:17:38 +0100

    added gitk. it's good for inspecting commits

--- install-misc.sh
+++ install-misc.sh
@@ -3,6 +3,7 @@ sudo apt-get install -y evince
 sudo apt-get install -y pandoc
 sudo apt-get install -y pidgin
 sudo apt-get install -y gitg
+sudo apt-get install -y gitk
 sudo apt-get install -y kdiff3
 sudo apt-get install -y tree
 sudo apt-get install -y shutter
