66becc9b01fb7be2f417fd3b85622175918a393b
jacobstanley <jacob.stanley@purenet.co.uk>
2015-08-03 10:14:59 +0100

    added dotfiles.sh permissions

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,29 +1,29 @@
-rm -r ~/.bash_logout
-rm -r ~/.bashrc
-rm -r ~/.config
-rm -r ~/.gitconfig
-rm -r ~/.gtkrc-2.0
-rm -r ~/.i3
-rm -r ~/.inputrc
-rm -r ~/.profile
-rm -r ~/.shutter
-rm -r ~/.themes
-rm -r ~/.xinitrc
-rm -r ~/.Xresources
+sudo rm -r ~/.bash_logout
+sudo rm -r ~/.bashrc
+sudo rm -r ~/.config
+sudo rm -r ~/.gitconfig
+sudo rm -r ~/.gtkrc-2.0
+sudo rm -r ~/.i3
+sudo rm -r ~/.inputrc
+sudo rm -r ~/.profile
+sudo rm -r ~/.shutter
+sudo rm -r ~/.themes
+sudo rm -r ~/.xinitrc
+sudo rm -r ~/.Xresources
 
 # Create symbolic links
-ln -s ~/conf/dotfiles/.bash_logout ~/.bash_logout
-ln -s ~/conf/dotfiles/.bashrc ~/.bashrc
-ln -s ~/conf/dotfiles/.config ~/.config
-ln -s ~/conf/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/conf/dotfiles/.gtkrc-2.0 ~/.gtkrc-2.0
-ln -s ~/conf/dotfiles/.i3 ~/.i3
-ln -s ~/conf/dotfiles/.inputrc ~/.inputrc
-ln -s ~/conf/dotfiles/.profile ~/.profile
-ln -s ~/conf/dotfiles/.shutter ~/.shutter
-ln -s ~/conf/dotfiles/.themes ~/.themes
-ln -s ~/conf/dotfiles/.xinitrc ~/.xinitrc
-ln -s ~/conf/dotfiles/.Xresources ~/.Xresources
+sudo ln -s ~/conf/dotfiles/.bash_logout ~/.bash_logout
+sudo ln -s ~/conf/dotfiles/.bashrc ~/.bashrc
+sudo ln -s ~/conf/dotfiles/.config ~/.config
+sudo ln -s ~/conf/dotfiles/.gitconfig ~/.gitconfig
+sudo ln -s ~/conf/dotfiles/.gtkrc-2.0 ~/.gtkrc-2.0
+sudo ln -s ~/conf/dotfiles/.i3 ~/.i3
+sudo ln -s ~/conf/dotfiles/.inputrc ~/.inputrc
+sudo ln -s ~/conf/dotfiles/.profile ~/.profile
+sudo ln -s ~/conf/dotfiles/.shutter ~/.shutter
+sudo ln -s ~/conf/dotfiles/.themes ~/.themes
+sudo ln -s ~/conf/dotfiles/.xinitrc ~/.xinitrc
+sudo ln -s ~/conf/dotfiles/.Xresources ~/.Xresources
 
 # Refresh
 source ~/.bashrc
