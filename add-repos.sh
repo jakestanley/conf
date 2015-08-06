56133907e7b6088912fc7364aec18f527b077ba7
jacobstanley <jacob.stanley@purenet.co.uk>
2015-08-06 10:08:22 +0100

    moved more repos into add-repos.sh

--- add-repos.sh
+++ add-repos.sh
@@ -1,7 +1,18 @@
 # Adds necessary keys and repositories
 
-## Spotify
-sudo echo "deb http://repository.spotify.com stable non-free" | sudo tee -a /etc/apt/sources.list
+# Flux
+sudo add-apt-repository ppa:kilian/f.lux
+
+# TLP
+sudo add-apt-repository ppa:linrunner/tlp
+
+# Spotify
+sudo apt-add-repository -y "deb http://repository.spotify.com stable non-free"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
 
+# Mopidy
+wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
+sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/mopidy.list
+
+# Update
 sudo apt-get update
