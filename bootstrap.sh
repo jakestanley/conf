07dd0808b054ef2ba137804e1947260616110b5f
jacobstanley <jacob.stanley@purenet.co.uk>
2015-08-12 09:43:29 +0100

    ignored, deleted, and added some bs

--- bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Downloads and installs Git on a vanilla Ubuntu installation and checks out the project
-sudo apt-get update
-sudo apt-get install -y git
-cd ~
-git clone https://github.com/madstanners/conf.git
