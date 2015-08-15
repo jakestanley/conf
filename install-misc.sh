54e444e8443e803fa53f9ac2b727fb4eb90485ae
jacobstanley <jacob.stanley@purenet.co.uk>
2015-08-15 21:50:07 +0100

    added redshift-gtk

--- install-misc.sh
+++ install-misc.sh
@@ -12,7 +12,7 @@ sudo apt-get install -y shutter
 sudo apt-get install -y lxappearance
 sudo apt-get install -y compton
 sudo apt-get install -y xsel
-sudo apt-get install -y redshift
+sudo apt-get install -y redshift redshift-gtk
 sudo apt-get install -y htop
 sudo apt-get install -y ultra-flat-icons
 sudo apt-get install -y ultra-flat-icons-green
